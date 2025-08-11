cf5bc895f68c887877684dae57169cc38124d570
solevaler68-ux <solevaler68@gmail.com>
2025-08-11 21:28:18 +0300

    второй комит

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import random
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+number = random.randint(1, 100)
+
+guess = int(input("Make a guess: "))
+
+if guess == number:
